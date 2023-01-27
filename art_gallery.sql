8173ab125b63e4600c304f30035f8f5bec8e524e
TT_WidowMaker <ricttt3@googlemail.com>
2023-01-27 22:15:30 +0000

    Update art_gallery.sql

--- art_gallery.sql
+++ art_gallery.sql
@@ -1,27 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 27, 2023 at 05:49 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `art_gallery`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `artworks`
@@ -40,15 +17,7 @@ CREATE TABLE `artworks` (
   `featured` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `artworks`
---
 
-INSERT INTO `artworks` (`id`, `title`, `artist`, `description`, `image`, `rating`, `user_id`, `status`, `created_at`, `featured`) VALUES
-(7, 'Avatar', 'TT_WidowMaker', 'Avatar PFP', 'lN2XxFjED06RByPTwrmr--3--kis0h.jpg', 0, 5, 'approved', '2023-01-26 17:42:57', 0),
-(8, 'Avatar', 'TT_WidowMaker', 'Avatar PFP', 'JLEEIFOOKEK3llChdh49--1--htik1.jpg', 0, 5, 'approved', '2023-01-26 21:06:35', 0);
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `tokens`
@@ -60,14 +29,6 @@ CREATE TABLE `tokens` (
   `token_balance` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tokens`
---
-
-INSERT INTO `tokens` (`id`, `email`, `token_balance`) VALUES
-(1, 'ricttt3@gmail.com', 0);
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
@@ -81,16 +42,6 @@ CREATE TABLE `users` (
   `role` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
-(5, 'TT_WidowMaker', 'ricttt3@gmail.com', '$2y$10$NwG3UIRTJQwnRmZUMP9aAOatV4zgctu9LpHSHeedlWkjYGG/1i.PK', 'admin');
-
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `artworks`
