0567727fc06423f3afe10327710379b6f72a62a9
TT_WidowMaker <ricttt3@googlemail.com>
2023-01-30 21:51:31 +0000

    Update review-artwork.php

--- review-artwork.php
+++ review-artwork.php
@@ -1,9 +1,20 @@
 <?php
+
+session_start();
+
+// Include necessary files
+include 'db.php';
+
+$user_id = $_SESSION['user_id'];
+$query = "SELECT username FROM users WHERE id = '$user_id'";
+$result = mysqli_query($conn, $query);
+$username = mysqli_fetch_assoc($result)['username'];
+
 if (isset($_POST['submit'])) {
-    $username = $_SESSION['username'];
+    $user_id = $_SESSION['user_id'];
     $artwork_id = $_GET['id'];
     $review = $_POST['review'];
-    $query = "INSERT INTO reviews (username, artwork_id, review) VALUES ('$username', '$artwork_id', '$review')";
+    $query = "INSERT INTO reviews (user_id, artwork_id, review) VALUES ('$user_id', '$artwork_id', '$review')";
     mysqli_query($conn, $query);
     header('location: index.php?page=artwork-detail&id=' . $artwork_id);
 }
@@ -13,4 +24,3 @@ if (isset($_POST['submit'])) {
     <textarea name="review"></textarea>
     <input type="submit" name="submit" value="Submit">
 </form>
-
