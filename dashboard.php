ea6523be434aa899112058d60f4998d9f584dacf
TT_WidowMaker <ricttt3@googlemail.com>
2023-01-31 16:51:46 +0000

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,66 +1,141 @@
 <?php
+// Connect to the database
+include 'db.php';
 
-$user_id = $_SESSION['user_id'];
-$user_role = getRole($user_id);
+// Get all the artworks from the database
+$query = "SELECT * FROM artworks";
+$result = mysqli_query($conn, $query);
 
-if ($user_role != 'admin' && $user_role != 'artist') {
-    header('Location: index.php');
-    exit;
+// Get the total number of users
+$query = "SELECT COUNT(*) AS total_users FROM users";
+$result_users = mysqli_query($conn, $query);
+$total_users = mysqli_fetch_assoc($result_users)['total_users'];
+
+// Get the stats on artworks submitted and rejected
+$query = "SELECT COUNT(*) AS total, status FROM artworks GROUP BY status";
+$result_stats = mysqli_query($conn, $query);
+$stats = [];
+while ($row = mysqli_fetch_assoc($result_stats)) {
+    $stats[$row['status']] = $row['total'];
+
+// Get the total number of users from the database
+$query = "SELECT COUNT(*) as total FROM users";
+$result_users = mysqli_query($conn, $query);
+$users = mysqli_fetch_assoc($result_users);
+$total_users = $users['total'];
+
+// Get the average rating of artworks from the database
+$query = "SELECT AVG(rating) as average_rating FROM artworks";
+$result_rating = mysqli_query($conn, $query);
+$rating = mysqli_fetch_assoc($result_rating);
+$average_rating = $rating['average_rating'];
+
+// Get the registered users by month from the database
+$query = "SELECT COUNT(*) as total, MONTH(created_at) as month, YEAR(created_at) as year FROM users GROUP BY MONTH(created_at), YEAR(created_at)";
+$result_users_by_month = mysqli_query($conn, $query);
+$users_by_month = [];
+while ($row = mysqli_fetch_assoc($result_users_by_month)) {
+    $users_by_month[] = [
+        'month' => date("F", mktime(0, 0, 0, $row['month'], 10)),
+        'year' => $row['year'],
+        'total' => $row['total']
+    ];
 }
 
-$user = getUser($user_id);
-$username = $user['username'];
-
-if ($user_role == 'admin') {
-    $artworks = getAllArtworks();
-} else {
-    $artworks = getArtworksByArtist($user_id);
 }
-
 ?>
-<h1>Welcome, <?php echo $username; ?></h1>
-<h2>Your Artworks</h2>
 
-<table>
-    <tr>
-        <th>Title</th>
-        <th>Image</th>
-        <th>Description</th>
-        <th>Actions</th>
-    </tr>
-    <?php foreach ($artworks as $artwork): ?>
-    <tr>
-        <td><?php echo $artwork['title']; ?></td>
-        <td>
-        <?php 
-        $ext = pathinfo($artwork['image'], PATHINFO_EXTENSION);
-        switch ($ext) {
-            case 'jpg':
-            case 'jpeg':
-            case 'png':
-                echo '<img src="images/'.htmlspecialchars($artwork['image'], ENT_QUOTES).'" alt="'.$artwork['title'].'" width="100">';
-                break;
-            case 'mp4':
-                echo '<div class="card-video">
-                        <video width="100" height="100" controls>
-                            <source src="images/'.htmlspecialchars($artwork['image'], ENT_QUOTES).'" type="video/mp4">
-                            Your browser does not support the video tag.
-                        </video>
-                      </div>';
-                break;
-            default:
-                echo 'File type not supported';
-                break;
+
+<!-- Show the total number of users -->
+<h3>Total Users: <?php echo $total_users; ?></h3>
+
+<!-- Show the stats on artworks submitted and rejected -->
+<h3>Artwork Stats:</h3>
+<ul>
+    <li>Submitted: <?php echo isset($stats['approved']) ? $stats['approved'] : 0; ?></li>
+    <li>Rejected: <?php echo isset($stats['rejected']) ? $stats['rejected'] : 0; ?></li>
+</ul>
+
+<!-- Display the total number of registered users and average rating of artworks -->
+<div class="stats">
+  <p>Total Registered Users: <?php echo $total_users; ?></p>
+  <p>Average Rating of Artworks: <?php echo $average_rating; ?></p>
+</div>
+
+
+<!-- Show all the artworks in a card style -->
+<h3>All Artworks:</h3>
+<div class="row">
+    <?php while ($artwork = mysqli_fetch_assoc($result)): ?>
+        <div class="col-sm-4">
+            <div class="card">
+                <img src="images/<?php echo $artwork['image']; ?>" class="card-img-top" alt="Artwork Image">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $artwork['title']; ?></h5>
+                    <p class="card-text">Artist: <?php echo $artwork['artist']; ?></p>
+                    <p class="card-text">Status: <?php echo $artwork['status']; ?></p>
+                    <a href="index.php?page=edit-artwork&id=<?php echo $artwork['id']; ?>" class="btn btn-primary">Edit</a>
+                    <a href="index.php?page=delete-artwork&id=<?php echo $artwork['id']; ?>" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this artwork?');">Delete</a>
+                </div>
+            </div>
+        </div>
+    <?php endwhile; ?>
+</div>
+
+<script>
+    function approve(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'update-status.php', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.response);
+            if(response.status === 'success') {
+                location.reload();
+            } else {
+                alert(response.message);
+            }
         }
-        ?>
-        </td>
-        <td><?php echo $artwork['description']; ?></td>
-        <td>
-            <?php if ($user_role == 'admin' || $user_id == $artwork['artist_id']): ?>
-                <a href="edit-artwork.php?id=<?php echo $artwork['id']; ?>">Edit</a>
-                <a href="delete-artwork.php?id=<?php echo $artwork['id']; ?>" onclick="return confirm('Are you sure you want to delete this artwork?');">Delete</a>
-            <?php endif; ?>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-</table>
+    }
+    xhr.send('id=' + id + '&status=approved');
+}
+
+function reject(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'update-status.php', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var response = JSON.parse(xhr.response);
+            if(response.status === 'success') {
+                location.reload();
+            } else {
+                alert(response.message);
+            }
+        }
+    }
+    xhr.send('id=' + id + '&status=rejected');
+}
+
+function deleteArtwork(id) {
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'delete-artwork.php', true);
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+xhr.onreadystatechange = function() {
+if (xhr.readyState === 4 && xhr.status === 200) {
+var response = JSON.parse(xhr.response);
+if(response.status === 'success') {
+location.reload();
+} else {
+alert(response.message);
+}
+}
+}
+xhr.send('id=' + id);
+}
+
+</script>
+
+
+
+
